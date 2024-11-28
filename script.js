9481048270af5e69f026fd242aa702fb2216b83a
sophie <sogomispro@gmail.com>
2024-11-28 18:01:41 +0100

    clean du code

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ likeButton.addEventListener("click", () => {
 
   // Si l'icône est un cœur vide
   if (icon.classList.contains("fa-regular")) {
-    icon.classList.replace("fa-regular", "fa-solid"); // Remplacer fa-regular par fa-solid
+    icon.classList.replace("fa-regular", "fa-solid"); // Remplacer fa-regular par fa-solid (coeur plein)
     icon.classList.add("red-heart"); // Ajouter la couleur rouge
   } else {
-    icon.classList.replace("fa-solid", "fa-regular"); // Remplacer fa-solid par fa-regular
+    icon.classList.replace("fa-solid", "fa-regular"); // Remplacer fa-solid par fa-regular (coeur vide)
     icon.classList.remove("red-heart"); // Retirer la couleur rouge
   }
 });
